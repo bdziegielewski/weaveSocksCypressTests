5e446a139fa47da6376d64dcffc026ac65767e5c
CydPL <dziegielewski.b@gmail.com>
2021-05-01 11:50:51 +0200

    added user registration cases

--- cypress/integration/user/register.spec.js
+++ cypress/integration/user/register.spec.js
@@ -8,28 +8,36 @@ context('Tests related to existance of user', () => {
         })
     })
 
-    it('New user should register successfully', () => {
-        // const user = users.user[0]
-        // openModalAndFillLoginData(user.login, user.password)
-        // cy.contains('Log in').click()
-        // cy.contains('Login successful.').should('exist')
-        // assertUserLoggedIn(user)
+    it('New user should register and logged in successfully', () => {
+        var user = users.user[3]
+        user.login = user.login + Math.random() // assuming that app's DB isn't rolled back every time tests are executed
+        registerUser(user)
+        assertUserLoggedIn(user)
     })
 
-    it('New user should register successfully', () => {
+    it.only('New registered user can login successfully after logout', () => {
+        var user = users.user[3]
+        user.login = user.login + Math.random() // assuming that app's DB isn't rolled back every time tests are executed
+        registerUser(user)
+        assertUserLoggedIn(user)
+        cy.get('#logout').click()
+        cy.login(user.login, user.password)
+        assertUserLoggedIn(user)
+    })
+
+    it('User should not be able to use existing login name', () => {
         throw 'TODO: Fill the test'
     })
-    
-    it('New user should register successfully', () => {
-        throw 'TODO: Fill the test'
-    })
 
-
-
-    function openModalAndFillLoginData(login, password) {
-        cy.get('#login').click()
-        cy.get('#username-modal').click().type(login)
-        cy.get('#password-modal').click().type(password)
+    function registerUser(user) {
+        cy.get('#register').click()
+        cy.get('#register-username-modal').type(user.login)
+        cy.get('#register-first-modal').type(user.firstname)
+        cy.get('#register-last-modal').type(user.lastname)
+        cy.get('#register-email-modal').type(user.email)
+        cy.get('#register-password-modal').type(user.password)
+        cy.get('.btn').contains('Register').click()
+        cy.contains('Registration and login successful.').should('exist')
     }
 
     function assertUserLoggedIn(user) {
